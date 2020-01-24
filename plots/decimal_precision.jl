d88015e27c0168132fcef861ce3bf9093e401c2c
milankl <milankloewer@gmx.de>
2020-01-24 21:06:53 +0000

    Q6.10 position corrected

--- plots/decimal_precision.jl
+++ plots/decimal_precision.jl
@@ -3,7 +3,7 @@ using SoftPosit, PyPlot, Lorenz63, StatsBase, Statistics
 include("representable_numbers.jl")
 include("lorenz_data.jl")
 
-N = 1_000_000
+N = 100_000
 L63data_s0 = lorenz_data(N,1/10)
 L63data_s1 = lorenz_data(N,1.)
 L63data_s2 = lorenz_data(N,10.)
@@ -21,7 +21,6 @@ posit16_2 = Float64.(Posit16_2.(UInt16.(collect(1:32767))));
 p1_am, p1_wda = wcdp_posit(posit16)
 p2_am, p2_wda = wcdp_posit(posit16_2)
 
-##
 i_am = vcat(0.5000001,1.49999999:1:(2^15-1))
 i_wda = decprec(i_am,round.(i_am))
 i_am[1] = 1
@@ -46,12 +45,12 @@ ax1.plot(bf_am,bf_wda,"0.7",lw=1.4)
 ax1.plot(p1_am,p1_wda,"#50C070",lw=1.2)
 ax1.plot(p2_am,p2_wda,"#900000",lw=0.8)
 ax1.plot(i_am,i_wda,"C0",lw=2)
-ax1.plot(i_am/100,i_wda,"#00E0E0",lw=2)
+ax1.plot(i_am/2^10,i_wda,"C1",lw=2)
 
 ax1.fill_between(f_am,-0.1,f_wda,edgecolor="k",facecolor="none",linestyle="--")
 ax1.fill_between(i_am,-0.1,i_wda,edgecolor="C0",facecolor="none",linestyle="--")
-ax1.fill_between(i_am/100,-0.1,i_wda,edgecolor="#00E0E0",facecolor="none",linestyle="--")
-ax1.fill_between(p1_am,-0.1,p1_wda,where=((p1_am .>= p1list[1]).*(p1_am .<= p1list[end])),edgecolor="C2",facecolor="none",linestyle="--")
+ax1.fill_between(i_am/2^10,-0.1,i_wda,edgecolor="C1",facecolor="none",linestyle="--")
+ax1.fill_between(p1_am,-0.1,p1_wda,where=((p1_am .>= posit16[1]).*(p1_am .<= posit16[end])),edgecolor="C2",facecolor="none",linestyle="--")
 
 # ax1.legend(loc=2,fontsize=9)
 
@@ -71,7 +70,7 @@ ax1.text(5e-15,2,"Posit(16,2)",color="#900000")
 ax1.text(5e-7,4.1,"Float16",color="k")
 ax1.text(1e-15,3.2,"BFloat16",color="0.3")
 ax1.text(6e4,5,"Int16",color="C0")
-ax1.text(30,5.7,"Q6.10",color="#00E0E0",rotation=65)
+ax1.text(30,5.3,"Q6.10",color="C1",rotation=0)
 
 # ax11 = ax1[:twiny]()
 # ax11[:set_xscale]("log",basex=10)
@@ -100,5 +99,5 @@ ax1.set_title("a",loc="right",fontweight="bold")
 ax2.set_title("b",loc="right",fontweight="bold")
 
 tight_layout()
-savefig("decimal_precision.png",dpi=300)
+savefig("plots/decimal_precision.png",dpi=300)
 close(fig)
