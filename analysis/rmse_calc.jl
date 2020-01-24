caa6cea7b0a237c528708d531b20e01efdf3876e
milankl <milankloewer@gmx.de>
2020-01-24 20:50:31 +0000

    error growth and snapshot

--- analysis/rmse_calc.jl
+++ analysis/rmse_calc.jl
@@ -3,96 +3,72 @@ using JLD2
 using FileIO
 using Printf
 using Statistics
+using StatsBase
 
-#path = "/network/aopp/cirrus/pred/kloewer/julsdata/forecast2/"
-path = "/network/aopp/chaos/pred/kloewer/julsdata/forecast/"
+path = "/network/aopp/chaos/pred/kloewer/julsdata/forecast2/"
 
 nn = 8       # number of number types to compare
 nt = 404     # number of time steps
-ne = 270     # number of ensemble members
-
+ne = 300     # number of ensemble members
 R = Array{Float64,3}(undef,nn,nt,ne)
 
-#failed_runs = [9,21,23,24,29,42,43,47,58,68,93,99,109,116,117,122,
-                # 139,144,147,150,152,168,190,195,204,211,216,220,223,234,
-                # 242,247,251,267,269,271,273,282,283,289,297,301,310,
-                # 311,317,319,322,324,328,329,330,345,346,352,356,361,
-                # 366,372,376,380,401,406]
+var = "v"
 
-#failed_runs = [84,251,252,255,256,257,258,259,260,261,262,263,265,266,267,268,269]
-failed_runs = [84,182,183,184,185,186,187,188,192,251,252,255,256,257,258,259,260,261,262,263,265,266,267,268,269]
+for i in 0:ne-1
 
-io = 0  # index for output
-ie = 0  # index for runs
+    println("Ensemble member $i")
 
-while io < ne    # loop over ensemble member
-    if ie in failed_runs
-        println("Skipping failed ensemble member $ie")
-    else
-        global io += 1
+    run_id = "run"*@sprintf("%04d",i)
 
-        println("Ensemble member $ie")
+    # READ TRUTH
+    nc = NetCDF.open(joinpath(path,"Float64",run_id,var*".nc"))
+    F64 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        run_id = "run"*@sprintf("%04d",ie)
+    # Persistence forecast
+    F64p = repeat(F64[:,:,1],1,1,size(F64)[3])
 
-        # READ TRUTH
-        nc = NetCDF.open(path*"Float64/"*run_id*"/eta.nc")
-        F64 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
+    # READ F64 with Sadourny/RK3
+    nc = NetCDF.open(joinpath(path,"Float64_RK3_Sad",run_id,var*".nc"))
+    F64RK3 = nc.vars[var][:,:,:]
+    # due to differnt time stepping one time step less, copy last time step
+    F64RK3 = cat(F64RK3,F64RK3[:,:,end],dims=3)
+    NetCDF.close(nc)
 
-        # Persistence forecast
-        F64p = repeat(F64[:,:,1],1,1,size(F64)[3])
+    nc = NetCDF.open(joinpath(path,"Float32",run_id,var*".nc"))
+    F32 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        # READ F64 with Sadourny/RK3
-        nc = NetCDF.open(path*"Float64Sad/"*run_id*"/eta.nc")
-        F64S = nc.vars["eta"][:,:,:]
-        #F64S = cat(F64S,F64S[:,:,end],dims=3)   # due to differnt time stepping one time step less, copy...
-        NetCDF.close(nc)
+    nc = NetCDF.open(joinpath(path,"Float16",run_id,var*".nc"))
+    F16 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        # READ F16
-        nc = NetCDF.open(path*"Float16/"*run_id*"/eta.nc")
-        F16 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
+    nc = NetCDF.open(joinpath(path,"Posit16",run_id,var*".nc"))
+    P16 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        # READ F32
-        nc = NetCDF.open(path*"Float32/"*run_id*"/eta.nc")
-        F32 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
+    nc = NetCDF.open(joinpath(path,"Posit16_2",run_id,var*".nc"))
+    P162 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        # READ POSIT{16,0}
-        nc = NetCDF.open(path*"Posit160/"*run_id*"/eta.nc")
-        P160 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
+    nc = NetCDF.open(joinpath(path,"Float16_32",run_id,var*".nc"))
+    F1632 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        # READ POSIT{16,1}
-        nc = NetCDF.open(path*"Posit161/"*run_id*"/eta.nc")
-        P161 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
+    nc = NetCDF.open(joinpath(path,"BFloat16_32",run_id,var*".nc"))
+    BF1632 = nc.vars[var][:,:,:]
+    NetCDF.close(nc)
 
-        # READ POSIT{16,2}
-        nc = NetCDF.open(path*"Posit162/"*run_id*"/eta.nc")
-        P162 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
-
-        # READ POSIT{16,3}
-        nc = NetCDF.open(path*"Posit163/"*run_id*"/eta.nc")
-        P163 = nc.vars["eta"][:,:,:]
-        NetCDF.close(nc)
-
-        # Compute RMSEs - average over space, time is last dim
-        R[1,:,io] = sqrt.(mean((F64-F64S).^2,dims=(1,2)))[1,1,:]
-        R[2,:,io] = sqrt.(mean((F64-F16).^2,dims=(1,2)))[1,1,:]
-        R[3,:,io] = sqrt.(mean((F64-F32).^2,dims=(1,2)))[1,1,:]
-        R[4,:,io] = sqrt.(mean((F64-F64p).^2,dims=(1,2)))[1,1,:]
-        R[5,:,io] = sqrt.(mean((F64-P160).^2,dims=(1,2)))[1,1,:]
-        R[6,:,io] = sqrt.(mean((F64-P161).^2,dims=(1,2)))[1,1,:]
-        R[7,:,io] = sqrt.(mean((F64-P162).^2,dims=(1,2)))[1,1,:]
-        R[8,:,io] = sqrt.(mean((F64-P163).^2,dims=(1,2)))[1,1,:]
-    end
-
-    global ie += 1
-
+    # Compute RMSEs - average over space, time is last dim
+    R[1,:,i+1] = sqrt.(mean((F64-F64p).^2,dims=(1,2)))[1,1,:]
+    R[2,:,i+1] = sqrt.(mean((F64-F64RK3).^2,dims=(1,2)))[1,1,:]
+    R[3,:,i+1] = sqrt.(mean((F64-F32).^2,dims=(1,2)))[1,1,:]
+    R[4,:,i+1] = sqrt.(mean((F64-F16).^2,dims=(1,2)))[1,1,:]
+    R[5,:,i+1] = sqrt.(mean((F64-P16).^2,dims=(1,2)))[1,1,:]
+    R[6,:,i+1] = sqrt.(mean((F64-P162).^2,dims=(1,2)))[1,1,:]
+    R[7,:,i+1] = sqrt.(mean((F64-F1632).^2,dims=(1,2)))[1,1,:]
+    R[8,:,i+1] = sqrt.(mean((F64-BF1632).^2,dims=(1,2)))[1,1,:]
 end
 
 # OUTPUT
-save(path*"analysis/RMSE_270P3.jld2","RMSE",R)
+save("RMSE_$var.jld2","RMSE",R)
--- plots/snapshot.jl
+++ plots/snapshot.jl
@@ -2,58 +2,98 @@ using NetCDF
 using PyPlot
 using PyCall
 
-@pyimport cmocean.cm as cm
-#@pyimport matplotlib.pyplot as plt
+cm = pyimport("cmocean.cm")
 
-#plt[:rcParams]["mathtext.fontset"] = "cm"
-#plt[:rcParams]["mathtext.rm"] = "serif"
+path = "/network/aopp/chaos/pred/kloewer/julsdata/forecast2/nx400/"
+time_step = 201
 
-#path = "/network/aopp/chaos/pred/kloewer/forecast2/"
-path = "/network/aopp/chaos/pred/kloewer/julsdata/ssthr/"
-run_id = "run"*@sprintf("%04d",3)
+run_id1 = "run0001"
+run_id2 = "run0002"
+run_id3 = "run0003"
+run_id4 = "run0004"
+run_id5 = "run0001"
+run_id6 = "run0001"
 
-i = 100
 
-# READ TRUTH
-nc = NetCDF.open(path*run_id*"/sst.nc")
-sst = reshape(nc.vars["sst"][:,:,i],800,400)
+# READ DATA
+nc = NetCDF.open(joinpath(path,run_id1,"sst.nc"))
+sst1 = nc.vars["sst"][:,:,time_step]
 x = nc.vars["x"][:]
 y = nc.vars["y"][:]
+sst1 = reshape(sst1,length(x),length(y))
 NetCDF.close(nc)
 
-## SSTA
-xx,yy = meshgrid(x,y)
-SSTmax = 30.
-SSTmin = 0.
-SSTw = 1000e3
-SSTϕ = 0.5
-Ly = 4000e3
-sstm = (SSTmin+SSTmax)/2 .+ tanh.(2π*(Ly/(4*SSTw))*(yy/Ly .- SSTϕ))*(SSTmin-SSTmax)/2
-ssta = sst - sstm
-## Plot
+nc = NetCDF.open(joinpath(path,run_id2,"sst.nc"))
+sst2 = reshape(nc.vars["sst"][:,:,time_step],length(x),length(y))
+NetCDF.close(nc)
 
-levs = 0:0.2:30
-#clevs = 27:.2:30
+nc = NetCDF.open(joinpath(path,run_id3,"sst.nc"))
+sst3 = reshape(nc.vars["sst"][:,:,time_step],length(x),length(y))
+NetCDF.close(nc)
+
+nc = NetCDF.open(joinpath(path,run_id4,"sst.nc"))
+sst4 = reshape(nc.vars["sst"][:,:,time_step],length(x),length(y))
+NetCDF.close(nc)
+
+nc = NetCDF.open(joinpath(path,run_id5,"sst.nc"))
+sst5 = reshape(nc.vars["sst"][:,:,time_step],length(x),length(y))
+NetCDF.close(nc)
+
+nc = NetCDF.open(joinpath(path,run_id6,"sst.nc"))
+sst6 = reshape(nc.vars["sst"][:,:,time_step],length(x),length(y))
+NetCDF.close(nc)
+
+## Plot
+levs = collect(0:0.05:1)
 
 ioff()
-fig,ax = subplots(1,1,figsize=(6,3))
-tight_layout(rect=[-0.06,-0.09,0.91,0.99])
-pos1 = ax[:get_position]()
-cax = fig[:add_axes]([pos1[:x1]+0.01,pos1[:y0],0.02,pos1[:y1]-pos1[:y0]])
+fig,axs = subplots(3,2,figsize=(8,7),sharex=true,sharey=true)
 
-q = ax[:contourf](x,y,sst',levs,cmap="magma",extend="both")
-#q = ax[:contourf](x,y,sst',levs,colors="k",linewidths=0.2)
-cb = colorbar(q,cax=cax)
-cb[:set_label]("[°C]")
-cb[:set_ticks]([0,5,10,15,20,25,30])
-#cb[:set_ticks]([-15,-10,-5,0,5,10,15])
+tight_layout(rect=[-.025,0.07,1,0.99],w_pad=0.03,h_pad=0.6)
+pos1 = axs[3,1].get_position()
+pos2 = axs[3,2].get_position()
+cax = fig.add_axes([pos1.x0,0.07,pos2.x1-pos1.x0,0.02])
 
-#ax[:set_ylabel](L"$y$")
-#ax[:set_xlabel](L"$x$")
-ax[:set_xticks]([])
-ax[:set_yticks]([])
-ax[:set_title]("Temperature anomaly simulated with 64bit floats",loc="left",fontsize=8)
+cmap = cm.tempo_r
 
-#savefig("/home/kloewer/julia/swm_rmse/snapshot_posit.png",dpi=300)
-savefig("/home/kloewer/juls/figs/sst_hr_float64.png",dpi=300)
+q = axs[1,1].contourf(x,y,sst1',levs,cmap=cmap)
+axs[2,1].contourf(x,y,sst2',levs,cmap=cmap)
+axs[3,1].contourf(x,y,sst3',levs,cmap=cmap)
+
+axs[1,2].contourf(x,y,sst4',levs,cmap=cmap)
+axs[2,2].contourf(x,y,sst5',levs,cmap=cmap)
+axs[3,2].contourf(x,y,sst6',levs,cmap=cmap)
+
+
+cb = colorbar(q,cax=cax,orientation="horizontal")
+cb.set_label("Tracer concentration")
+cb.set_ticks([0,0.2,0.4,0.6,0.8,1])
+
+axs[1,1].set_ylabel(L"$y$")
+axs[2,1].set_ylabel(L"$y$")
+axs[3,1].set_ylabel(L"$y$")
+axs[3,1].set_xlabel(L"$x$")
+axs[3,2].set_xlabel(L"$x$")
+
+axs[1,1].set_title("Float64",loc="left",fontsize=8)
+axs[2,1].set_title("Posit16",loc="left",fontsize=8)
+axs[3,1].set_title("Posit16_2",loc="left",fontsize=8)
+
+axs[1,2].set_title("Float16",loc="left",fontsize=8)
+axs[2,2].set_title("Mixed precision: Float16/Float32",loc="left",fontsize=8)
+axs[3,2].set_title("Mixed precision: BFloat16/Float32",loc="left",fontsize=8)
+
+axs[1,1].set_title("a",loc="right",fontsize=8,fontweight="bold")
+axs[2,1].set_title("b",loc="right",fontsize=8,fontweight="bold")
+axs[3,1].set_title("c",loc="right",fontsize=8,fontweight="bold")
+
+axs[1,2].set_title("d",loc="right",fontsize=8,fontweight="bold")
+axs[2,2].set_title("e",loc="right",fontsize=8,fontweight="bold")
+axs[3,2].set_title("f",loc="right",fontsize=8,fontweight="bold")
+
+
+axs[1,1].set_xticks([])
+axs[1,1].set_yticks([])
+
+savefig("plots/snapshot.png",dpi=300)
 close(fig)
