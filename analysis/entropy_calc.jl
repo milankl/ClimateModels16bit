9ebe69b432d9cc3dfc0690d4b95509e7aa567bff
milankl <milankloewer@gmx.de>
2020-01-28 19:42:01 +0000

    entropy long, manuscript changes

--- analysis/entropy_calc.jl
+++ analysis/entropy_calc.jl
@@ -4,19 +4,16 @@ using Printf
 using Statistics
 using StatsBase
 
-path = "/network/aopp/chaos/pred/kloewer/julsdata/forecast2/"
+path = "/network/aopp/chaos/pred/kloewer/julsdata/forecast2/long/"
 
-nn = 8      # number of number types to compare
-nt = 404    # number of time steps
-ne = 300    # number of ensemble members
+nn = 6      # number of number types to compare
+nt = 1613   # number of time steps
+ne = 20     # number of ensemble members
 nx = 100    # number of grid cells in x
 ny = 50     # number of grid cells in y
 R = Array{Float64,3}(undef,nn,nt,ne)
 
 vari = "sst"
-
-Δ = 1/40
-bins = collect(-Δ/2:Δ:1+Δ/2)
 
 for i in 0:ne-1
 
@@ -29,16 +26,16 @@ for i in 0:ne-1
     F64 = nc.vars[vari][:,:,:]
     NetCDF.close(nc)
 
-    # READ F64 with Sadourny/RK3
-    nc = NetCDF.open(joinpath(path,"Float64_RK3_Sad",run_id,vari*".nc"))
-    F64RK3 = nc.vars[vari][:,:,:]
-    # due to differnt time stepping one time step less, copy last time step
-    F64RK3 = cat(F64RK3,F64RK3[:,:,end],dims=3)
-    NetCDF.close(nc)
-
-    nc = NetCDF.open(joinpath(path,"Float32",run_id,vari*".nc"))
-    F32 = nc.vars[vari][:,:,:]
-    NetCDF.close(nc)
+    # # READ F64 with Sadourny/RK3
+    # nc = NetCDF.open(joinpath(path,"Float64_RK3_Sad",run_id,vari*".nc"))
+    # F64RK3 = nc.vars[vari][:,:,:]
+    # # due to differnt time stepping one time step less, copy last time step
+    # F64RK3 = cat(F64RK3,F64RK3[:,:,end],dims=3)
+    # NetCDF.close(nc)
+    #
+    # nc = NetCDF.open(joinpath(path,"Float32",run_id,vari*".nc"))
+    # F32 = nc.vars[vari][:,:,:]
+    # NetCDF.close(nc)
 
     nc = NetCDF.open(joinpath(path,"Float16",run_id,vari*".nc"))
     F16 = nc.vars[vari][:,:,:]
@@ -61,14 +58,13 @@ for i in 0:ne-1
     NetCDF.close(nc)
 
     # Compute - average over x & time (last dimension)
-    for (ri,M) in enumerate([F64,F64RK3,F32,F16,P16,P162,F1632,BF1632])
+    # for (ri,M) in enumerate([F64,F64RK3,F32,F16,P16,P162,F1632,BF1632])
+    for (ri,M) in enumerate([F64,F16,P16,P162,F1632,BF1632])
         for it in 1:nt
-            #H = fit(Histogram,M[:,:,it][:],bins)
-            #R[ri,it,i+1] = entropy(H.weights/sum(H.weights),2)
             R[ri,it,i+1] = entropy(M[:,:,it][:],2)/(nx*ny)*2
         end
     end
 end
 
 # OUTPUT
-save("entropy.jld2","entropy",R)
+save("entropy_long.jld2","entropy",R)
--- plots/entropy_plot.jl
+++ plots/entropy_plot.jl
@@ -3,7 +3,7 @@ using Statistics
 using StatsBase
 using PyPlot
 
-R = load("analysis/entropy.jld2")["entropy"]*2
+R = load("analysis/entropy_long.jld2")["entropy"]
 
 nT,n,ne = size(R)      # number of types, time steps, ensemble members
 p = 25
@@ -36,30 +36,31 @@ end
 ioff()
 fig,ax = subplots(1,1,figsize=(6,4))
 
-q = 2
+q = 3
 
-ax.plot(t,median(R[1,:,:],dims=2).^q,"C0",lw=3,label="Float64")
-ax.plot(t,median(R[4,:,:],dims=2).^q,"k",label="Float16")
-ax.plot(t,median(R[5,:,:],dims=2).^q,"#50C070",ls="--",label="Posit(16,1)")
-ax.plot(t,median(R[6,:,:],dims=2).^q,"#900000",ls="--",label="Posit(16,2)")
-ax.plot(t,median(R[8,:,:],dims=2).^q,"grey",ls="-.",label="BFloat16/Float32")
-ax.plot(t,median(R[7,:,:],dims=2).^q,"C1",ls="-.",label="Float16/Float32")
+ax.plot(t,mean(R[1,:,:],dims=2).^q,"C0",lw=3,label="Float64")
+ax.plot(t,mean(R[2,:,:],dims=2).^q,"k",label="Float16")
+ax.plot(t,mean(R[3,:,:],dims=2).^q,"#50C070",ls="--",label="Posit(16,1)")
+ax.plot(t,mean(R[4,:,:],dims=2).^q,"#900000",ls="--",label="Posit(16,2)")
+ax.plot(t,mean(R[6,:,:],dims=2).^q,"grey",ls="-.",label="BFloat16/Float32")
+ax.plot(t,mean(R[5,:,:],dims=2).^q,"C1",ls="-.",label="Float16/Float32")
 
 ax.fill_between(t,Rp[1,1,:].^q,Rp[2,1,:].^q,color="C0",alpha=0.2,label="Float64 ensemble",zorder=-1)
 
 ax.set_ylabel("normalized entropy")
 ax.set_xlabel("time [days]")
 
-ax.set_xlim(0,100)
+ax.set_xlim(0,300)
+ax.set_ylim(0,1)
 
-ytik = [0,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+ytik = [0,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 ax.set_yticks(ytik.^q)
 ax.set_yticklabels(string.(ytik))
 
 #ax.set_ylim(0,1)
 ax.set_title("Entropy of mixing", loc="left")
 #
-ax.legend(loc=2,ncol=2)
+ax.legend(loc=4,ncol=2)
 #
 # di = Dict("facecolor"=>"w","alpha"=>0.5,"pad"=>2)
 #
